82b85a1558bd67056b7555073fcd03596a36fe97
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2024-08-25 00:51:20 +0200

    Some error fix

--- src/lib/wa-cloud-api.ts
+++ src/lib/wa-cloud-api.ts
@@ -1,7 +1,7 @@
 import { WaCloudCredentials, WaCloudMessages } from "./core";
 
 const id: string = "978648727277884";
-const wa: WaCloudMessages = new WaCloudMessages(new WaCloudCredentials(1037821177784041, "EAAO33DpjonYBO0UWGLIzGTek0UmqbKkf9lDetUi4EVVXxumszjaFZB1yWA89tnp2UrZB1ydZB2eAzKExnZCIGWjIYwZCm1RuKTpzeUV9hU89rb6JURq6RLoyU27FZBlw4Agcryv9ZBh5dUUK0JsONkdffj5KMyrwZCCA7etlePwuddo51x0vbsy4WJ42IY0VTCHVugZDZD", 420322614490057, 336517706221288));
+const wa: WaCloudMessages = new WaCloudMessages(undefined);
 const a = async () => {
     const a = await wa.sendButtonMessage("258860842024", "Hozaaaah", [
         {
